5e13edd0be89abd78827b25d74bc431ed47e2695
Caique Borges <caiquetgr@gmail.com>
2018-07-22 09:07:29 -0300

    Creating first microservice and a Spring Cloud Config Server Project

---
+++ SpringMicroServices/RestfulWebservices/restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user values(1000, sysdate(), 'Caique')
+insert into user values(2000, sysdate(), 'Mireli')
+insert into user values(3000, sysdate(), 'Miguel')
+insert into post values(1111,'My First Post',1000)
+insert into post values(1112,'My Second Post',1000)
