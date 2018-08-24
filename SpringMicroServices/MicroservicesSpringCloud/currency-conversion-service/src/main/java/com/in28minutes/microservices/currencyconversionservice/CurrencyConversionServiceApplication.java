c38f37c23bfad82bd4fbb21338c9bfe475e9ae09
Caique Borges <caiquetgr@gmail.com>
2018-08-24 19:25:13 -0300

    Spring Cloud / Microservices finished

--- SpringMicroServices/MicroservicesSpringCloud/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ SpringMicroServices/MicroservicesSpringCloud/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -1,5 +1,6 @@
 package com.in28minutes.microservices.currencyconversionservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -12,5 +13,9 @@ public class CurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+    }
+
+    public Sampler defaultSampler(){
+        return Sampler.ALWAYS_SAMPLE;
     }
 }
--- SpringMicroServices/MicroservicesSpringCloud/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ SpringMicroServices/MicroservicesSpringCloud/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +19,17 @@ public class CurrencyExchangeController {
     @Autowired
     private ExchangeValueRepository repository;
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
         ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
 
         Integer port = Integer.valueOf(environment.getProperty("local.server.port"));
         exchangeValue.setPort(port);
+
+        logger.info("{}", exchangeValue);
+
         return exchangeValue;
     }
 
